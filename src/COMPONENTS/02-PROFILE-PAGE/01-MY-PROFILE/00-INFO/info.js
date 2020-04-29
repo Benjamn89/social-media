63182b67deb42da0a8fdfc643bb5b7c622ed1ea0
unknown <newbennytal@gmail.com>
2020-04-29 05:23:51 +0300

    Testing deploy...

--- src/COMPONENTS/02-PROFILE-PAGE/01-MY-PROFILE/00-INFO/info.js
+++ src/COMPONENTS/02-PROFILE-PAGE/01-MY-PROFILE/00-INFO/info.js
@@ -13,26 +13,32 @@ class Info extends Component {
   render() {
     // Short cut to the profile box state
     const profileBoxState = this.props.profileBoxState;
+    // Initial var for display the componenet
+    var infoDiv = null;
+    // Checking if the profileBoxState exists to render the info
+    if (profileBoxState.email) {
+      infoDiv = (
+        <div className="info-div">
+          <ProfileBox
+            profileUrl={profileBoxState.url}
+            fullName={profileBoxState.fullName}
+            pickImage={null}
+            click={null}
+            Location={Location}
+            locationText={profileBoxState.location}
+            Website={Website}
+            websiteText={profileBoxState.website}
+            Calender={Calender}
+            timeStampMonth={profileBoxState.timeStamp.month}
+            timeStampYear={profileBoxState.timeStamp.year}
+            openModal={null}
+          />
+        </div>
+      );
+    }
+
     console.log("Info -> REDNER!!!");
-    return (
-      <div className="info-div">
-        <ProfileBox
-          profileUrl={profileBoxState.url}
-          fullName={profileBoxState.fullName}
-          pickImage={null}
-          click={null}
-          Location={Location}
-          locationText={profileBoxState.location}
-          Website={Website}
-          websiteText={profileBoxState.website}
-          Calender={Calender}
-          timeStampMonth={profileBoxState.timeStamp.month}
-          timeStampYear={profileBoxState.timeStamp.year}
-          openModal={null}
-          fileInput={this.fileInput}
-        />
-      </div>
-    );
+    return infoDiv;
   }
 }
 
--- src/COMPONENTS/FUNCTIONS/profile-box.js
+++ src/COMPONENTS/FUNCTIONS/profile-box.js
@@ -6,7 +6,7 @@ const profileBox = (props) => {
     <div className="inside-sec-two">
       <img src={props.profileUrl} alt="profileImg" className="profile-img" />
       <p className="profilebox-name-title-p">{props.fullName}</p>
-      {props.children}
+
       <div className="wrap-img-pen">
         {props.Pen ? (
           <img
@@ -47,57 +47,3 @@ const profileBox = (props) => {
 };
 
 export default profileBox;
-
-/* <div className="inside-sec-two">
-<img
-  src={this.props.profileBoxState.url}
-  alt="profileImg"
-  className="profile-img"
-/>
-<p className="profilebox-name-title-p">
-  {this.props.profileBoxState.fullName}
-</p>
-<input
-  ref={(fileInput) => (this.fileInput = fileInput)}
-  type="file"
-  onChange={this.pickImage}
-  style={{ display: "none" }}
-/>
-<div className="wrap-img-pen">
-  <img
-    src={Pen}
-    className="profile-box-pen-image"
-    alt="penImg"
-    onClick={() => this.fileInput.click()}
-  />
-</div>
-<div className="profile-box-div-info">
-  <div className="row-div-inside">
-    <img src={Location} alt="location" />
-
-    <p className="location-p">
-      {this.props.profileBoxState.location}
-    </p>
-  </div>
-  <div className="row-div-inside">
-    <img src={Website} alt="website" />
-    <p className="website-p">
-      {" "}
-      {this.props.profileBoxState.website}{" "}
-    </p>
-  </div>
-  <div className="row-div-inside">
-    <img src={Calender} alt="calender" />
-    joined {this.props.profileBoxState.timeStamp.month}{" "}
-    {this.props.profileBoxState.timeStamp.year}
-  </div>
-  <div className="wrap-img-pen2">
-    <img
-      onClick={this.openModal}
-      className="edit-profile-bio"
-      src={Pen}
-      alt="penImg"
-    />
-  </div>
-</div>
-</div> */
